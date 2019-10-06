2480bbd382f335d29868fcde756d872b9ac98794
zahraBodaghi <Zbodaghi@d2l.lonestar.edu>
2019-10-06 11:03:05 -0500

    Topical Assginment

--- Topical Assignment 2/CustomerManagement/Customer.cs
+++ Topical Assignment 2/CustomerManagement/Customer.cs
@@ -8,24 +8,22 @@ namespace CustomerManagement
 {
     public class Customer
     {
-
         public Customer()
         {
 
         }
         #region Properties, Variables and Fields
-        public Customer(int Customerid)
+        public Customer(int customerid)
         {
-            this.CustomerId = Customerid;
+            this.CustomerId = customerid;
         }
         /// <summary>
         /// static variable which may be tracked as an instance count
         /// </summary>
         public static int InstantanceCount { get; set; }
-
         private string _lastName;
-        public string LastName
-        {
+        public string LastName 
+        { 
             get
             {
                 return _lastName;
@@ -37,8 +35,10 @@ namespace CustomerManagement
         }
         public string FirstName { get; set; }
         public string EmailAddress { get; set; }
+        public int CustomerId { get; private set; }
+        #endregion
         /// <summary>
-        /// concates the last & first names
+        /// concatenates the last & first names
         /// </summary>
         public string FullName
         {
@@ -51,20 +51,19 @@ namespace CustomerManagement
                 return LastName + FirstName;
             }
         }
-        public int CustomerId { get; }
+
         /// <summary>
         /// retrives a customer record based on the customer id
         /// </summary>
         /// <param name="customerid"> id of the record to be retrived </param>
         /// <returns></returns>
-        #endregion
-        public Customer Retrieve(int customerid) => new Customer();
+        
+        public Customer Retrieve(int customerid)=> new Customer();
         /// <summary>
         /// retrive a list of all customers
         /// </summary>
         /// <returns>the customer record </returns>
         public List<Customer> Retrieve() => new List<Customer>();
-        //Zahra: Capitalized the Save method. 10/5/2019 4:23 pm
         /// <summary>
         /// saves a single record to the database
         /// </summary>
@@ -72,7 +71,7 @@ namespace CustomerManagement
         /// <returns>list of customers</returns>
         public bool Save(Customer customer) => true;
         /// <summary>
-        /// examines the customer properties and confirms consist and valid date
+        /// examines the customer properties and confirms consist and valid data
         /// </summary>
         /// <returns>true if valid</returns>
         public bool Validate()
