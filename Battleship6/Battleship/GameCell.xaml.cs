6dc23e392a6ec1e0b1b319c4356efade57f7cb77
zahraBodaghi <Zbodaghi@d2l.lonestar.edu>
2019-11-03 22:05:22 -0600

    Completed

--- Battleship6/Battleship/GameCell.xaml.cs
+++ Battleship6/Battleship/GameCell.xaml.cs
@@ -10,7 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+/*
+ * Zahra Bodaghi
+ */
 namespace Battleship
 {
     /// <summary>
