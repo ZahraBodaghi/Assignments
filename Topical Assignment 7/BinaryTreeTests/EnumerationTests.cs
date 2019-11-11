d7073100cfa7d809ac48fe5410044540ba879cc1
zahraBodaghi <Zbodaghi@d2l.lonestar.edu>
2019-11-11 00:10:16 -0600

    Completed Assignment 7

---
+++ Topical Assignment 7/BinaryTreeTests/EnumerationTests.cs
@@ -0,0 +1,37 @@
+﻿using BinaryTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace BinaryTreeTests
+{
+    [TestClass]
+    public class EnumerationTests
+    {
+        [TestMethod]
+        public void Enumerator_Of_Single()
+        {
+            BinaryTree<int> tree = new BinaryTree<int>();
+
+            
+            //Act
+            var condition = tree.Contains(10);
+            //Assert
+            condition.ShouldBeFalse("Tree Should not contain 10 yet");
+
+            tree.Add(10);
+
+            //Act
+            var newCondition = tree.Contains(10);
+            //Assert
+            newCondition.ShouldBeTrue("Tree should be contain 10");
+
+            int count = 0;
+            foreach (int item in tree)
+            {
+                count++;
+                Assert.AreEqual(item, 10, "The item value should be 10");
+            }
+        }
+
+    }
+}
---
+++ Topical Assignment 7/RandomNumberDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+/*
+ * Zahra Bodaghi
+ */
+
+namespace RandomNumberDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zahra Bodaghi");
+            Console.WriteLine("Assignment 7");
+            Random randomNumber = new Random();
+            int[] numbers = new int[10];
+       
+            Console.WriteLine("As Loaded...");
+            var count = 0;
+            
+            //Creating 10 random number
+            while(count != 10)
+            {
+               numbers[count] = randomNumber.Next(0, 100);
+               count++;
+            }
+            // Displaying 10 random numbers
+            for(int item = 0; item < 10; item++)
+            {
+                Console.Write(numbers[item]);
+                Console.Write("  ");
+            }
+            //Sorting out the numbers & Displaying them
+            Console.WriteLine("");
+            Console.WriteLine("As retrived...");
+            Array.Sort(numbers);
+            for(int item = 1; item < 10; item++)
+            {
+                Console.Write(numbers[item]);
+                Console.Write("  ");
+            }
+
+        }
+    }
+}
