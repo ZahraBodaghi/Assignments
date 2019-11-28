187d84b8575a2fe52f9f11a72c49ae063c7c62c1
zahraBodaghi <Zbodaghi@d2l.lonestar.edu>
2019-11-28 16:11:40 -0600

    Completed Assignment 9

---
+++ Topical Assignment 9/WarpPanel/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WarpPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MainWindow_OnLocationChanged(Object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            Rec0.Width = rnd.Next(25, 201);
+            Rec1.Width = rnd.Next(25, 201);
+            Rec2.Width = rnd.Next(25, 201);
+            Rec3.Width = rnd.Next(25, 201);
+            Rec4.Width = rnd.Next(25, 201);
+            Rec5.Width = rnd.Next(25, 201);
+            Rec6.Width = rnd.Next(25, 201);
+            Rec7.Width = rnd.Next(25, 201);
+            Rec8.Width = rnd.Next(25, 201);
+            Rec9.Width = rnd.Next(25, 201);
+        }
+    }
+}
